4f894f514d824d482d80c6d8c500333a84f7f780
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-13 17:46:37 +0300

    feat(content): add two calibration passages

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -1,9 +1,13 @@
 import bostonTeaParty from "./boston-tea-party.json";
 import covalentBonding from "./covalent-bonding.json";
+import sonsAndDaughtersOfLiberty from "./sons-and-daughters-of-liberty.json";
+import stampAct1765 from "./stamp-act-1765.json";
 import storyOfAnHour from "./story-of-an-hour.json";
 
 export const rawPassages: unknown[] = [
   covalentBonding,
   bostonTeaParty,
   storyOfAnHour,
+  stampAct1765,
+  sonsAndDaughtersOfLiberty,
 ];
