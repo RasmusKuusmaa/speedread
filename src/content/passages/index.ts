77e5393889cfae4df8f25a6143b1dc15db17c35a
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-13 19:49:29 +0300

    feat(content): add a prison-like rampart literature passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -7,6 +7,7 @@ import ozGrayPrairie from "./oz-gray-prairie.json";
 import ozGreenRoom from "./oz-green-room.json";
 import ozSilverShoes from "./oz-silver-shoes.json";
 import poeBlackCatWalledUp from "./poe-black-cat-walled-up.json";
+import poeWilliamWilsonSchool from "./poe-william-wilson-school.json";
 import secretGardenGreenPoints from "./secret-garden-green-points.json";
 import secretGardenHundredRooms from "./secret-garden-hundred-rooms.json";
 import secretGardenMistressContrary from "./secret-garden-mistress-contrary.json";
@@ -34,4 +35,5 @@ export const rawPassages: unknown[] = [
   secretGardenSilentBungalow,
   secretGardenHundredRooms,
   poeBlackCatWalledUp,
+  poeWilliamWilsonSchool,
 ];
