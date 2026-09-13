7ac18e684018dadd66cb7037c58cc4fbc53ba5f3
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-13 13:22:17 +0300

    feat(content): add a non-technical expository seed passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -1,3 +1,4 @@
+import bostonTeaParty from "./boston-tea-party.json";
 import covalentBonding from "./covalent-bonding.json";
 
-export const rawPassages: unknown[] = [covalentBonding];
+export const rawPassages: unknown[] = [covalentBonding, bostonTeaParty];
