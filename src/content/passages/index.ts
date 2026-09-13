28b97a596b41ab211bc84e8f0f03e46278343d32
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-13 13:28:48 +0300

    feat(content): add a narrative seed passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -1,4 +1,9 @@
 import bostonTeaParty from "./boston-tea-party.json";
 import covalentBonding from "./covalent-bonding.json";
+import storyOfAnHour from "./story-of-an-hour.json";
 
-export const rawPassages: unknown[] = [covalentBonding, bostonTeaParty];
+export const rawPassages: unknown[] = [
+  covalentBonding,
+  bostonTeaParty,
+  storyOfAnHour,
+];
