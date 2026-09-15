c9cda87a25c33d992845f679f6920ab26ab0c5ac
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-16 01:01:11 +0300

    feat(content): section chapter 10 of Oz into the book

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -69,6 +69,12 @@ import ozC09S1 from "./oz-c09-s1.json";
 import ozC09S2 from "./oz-c09-s2.json";
 import ozC09S3 from "./oz-c09-s3.json";
 import ozC09S4 from "./oz-c09-s4.json";
+import ozC10S1 from "./oz-c10-s1.json";
+import ozC10S2 from "./oz-c10-s2.json";
+import ozC10S3 from "./oz-c10-s3.json";
+import ozC10S4 from "./oz-c10-s4.json";
+import ozC10S5 from "./oz-c10-s5.json";
+import ozC10S6 from "./oz-c10-s6.json";
 import ozGrayPrairie from "./oz-gray-prairie.json";
 import ozGreenRoom from "./oz-green-room.json";
 import ozSilverShoes from "./oz-silver-shoes.json";
@@ -190,6 +196,12 @@ export const rawPassages: unknown[] = [
   ozC09S2,
   ozC09S3,
   ozC09S4,
+  ozC10S1,
+  ozC10S2,
+  ozC10S3,
+  ozC10S4,
+  ozC10S5,
+  ozC10S6,
   ozGreenRoom,
   ozSilverShoes,
 ];
