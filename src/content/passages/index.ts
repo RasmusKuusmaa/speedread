ee19b890c60e55aad0d251a0354c32e8d0ab82e9
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-16 20:53:12 +0300

    feat(content): add The Wolf and the Crane passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -66,6 +66,7 @@ import aesopTravellersAndPlaneTree from "./aesop-travellers-and-plane-tree.json"
 import aesopTwoPots from "./aesop-two-pots.json";
 import aesopVenusAndCat from "./aesop-venus-and-cat.json";
 import aesopWildBoarAndFox from "./aesop-wild-boar-and-fox.json";
+import aesopWolfAndCrane from "./aesop-wolf-and-crane.json";
 import aesopWolfAndShepherd from "./aesop-wolf-and-shepherd.json";
 import aliceDownTheRabbitHole from "./alice-down-the-rabbit-hole.json";
 import aristotleNicomacheanVirtueHabit from "./aristotle-nicomachean-virtue-habit.json";
@@ -307,6 +308,7 @@ export const rawPassages: unknown[] = [
   aesopTwoPots,
   aesopVenusAndCat,
   aesopWildBoarAndFox,
+  aesopWolfAndCrane,
   aesopWolfAndShepherd,
   bruceAndTheSpider,
   aliceDownTheRabbitHole,
