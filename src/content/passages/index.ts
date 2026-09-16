45a16c82c2604385a175121ff370eb6dcb2f91de
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-16 19:14:12 +0300

    feat(content): add The Goose That Laid the Golden Eggs passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -4,6 +4,7 @@ import aesopCrowAndPitcher from "./aesop-crow-and-pitcher.json";
 import aesopFoxAndCrow from "./aesop-fox-and-crow.json";
 import aesopFoxAndGoat from "./aesop-fox-and-goat.json";
 import aesopFoxAndStork from "./aesop-fox-and-stork.json";
+import aesopGooseAndGoldenEggs from "./aesop-goose-and-golden-eggs.json";
 import aesopHareAndTortoise from "./aesop-hare-and-tortoise.json";
 import aesopLionAndMouse from "./aesop-lion-and-mouse.json";
 import aesopMilkmaidAndHerPail from "./aesop-milkmaid-and-her-pail.json";
@@ -181,6 +182,7 @@ export const rawPassages: unknown[] = [
   aesopFoxAndCrow,
   aesopFoxAndGoat,
   aesopFoxAndStork,
+  aesopGooseAndGoldenEggs,
   aesopHareAndTortoise,
   aesopLionAndMouse,
   aesopMilkmaidAndHerPail,
