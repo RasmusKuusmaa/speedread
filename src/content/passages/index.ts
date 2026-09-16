7693ab6abb74a43c863a407e8b93c25c90d6867f
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-16 19:05:56 +0300

    feat(content): add The Crow and the Pitcher passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -1,4 +1,5 @@
 import aLaconicAnswer from "./a-laconic-answer.json";
+import aesopCrowAndPitcher from "./aesop-crow-and-pitcher.json";
 import aesopFoxAndCrow from "./aesop-fox-and-crow.json";
 import aesopHareAndTortoise from "./aesop-hare-and-tortoise.json";
 import aesopLionAndMouse from "./aesop-lion-and-mouse.json";
@@ -170,6 +171,7 @@ import wrightFlyerHistory from "./wright-flyer-history.json";
 
 export const rawPassages: unknown[] = [
   aLaconicAnswer,
+  aesopCrowAndPitcher,
   aesopFoxAndCrow,
   aesopHareAndTortoise,
   aesopLionAndMouse,
