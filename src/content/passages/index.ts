94a2f2f92de2fbf5882f59d21c4e6b98468d20f2
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-18 00:30:36 +0300

    feat(content): add The Hound and the Fox passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -66,6 +66,7 @@ import aesopHareAndTortoise from "./aesop-hare-and-tortoise.json";
 import aesopHaresAndFrogs from "./aesop-hares-and-frogs.json";
 import aesopHawkKiteAndPigeons from "./aesop-hawk-kite-and-pigeons.json";
 import aesopHorseAndGroom from "./aesop-horse-and-groom.json";
+import aesopHoundAndFox from "./aesop-hound-and-fox.json";
 import aesopJackdawAndPigeons from "./aesop-jackdaw-and-pigeons.json";
 import aesopKidOnHousetop from "./aesop-kid-on-housetop.json";
 import aesopLabourerAndSnake from "./aesop-labourer-and-snake.json";
@@ -364,6 +365,7 @@ export const rawPassages: unknown[] = [
   aesopHaresAndFrogs,
   aesopHawkKiteAndPigeons,
   aesopHorseAndGroom,
+  aesopHoundAndFox,
   aesopJackdawAndPigeons,
   aesopKidOnHousetop,
   aesopLabourerAndSnake,
