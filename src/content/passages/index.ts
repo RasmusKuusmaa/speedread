3634d02322a137ec4f63c42bf9c89e269d5c9cba
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-18 00:58:00 +0300

    feat(content): add The Lion, the Mouse, and the Fox passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -84,6 +84,7 @@ import aesopLabourerAndSnake from "./aesop-labourer-and-snake.json";
 import aesopLambChasedByAWolf from "./aesop-lamb-chased-by-a-wolf.json";
 import aesopLamp from "./aesop-lamp.json";
 import aesopLionAndMouse from "./aesop-lion-and-mouse.json";
+import aesopLionMouseAndFox from "./aesop-lion-mouse-and-fox.json";
 import aesopLionessAndVixen from "./aesop-lioness-and-vixen.json";
 import aesopManAndTwoSweethearts from "./aesop-man-and-two-sweethearts.json";
 import aesopMiceInCouncil from "./aesop-mice-in-council.json";
@@ -403,6 +404,7 @@ export const rawPassages: unknown[] = [
   aesopLambChasedByAWolf,
   aesopLamp,
   aesopLionAndMouse,
+  aesopLionMouseAndFox,
   aesopLionessAndVixen,
   aesopManAndTwoSweethearts,
   aesopMiceInCouncil,
