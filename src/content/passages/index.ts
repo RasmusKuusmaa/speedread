4b0c1d55f101ab9decb950a2824cf9b4a629740c
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-18 00:32:57 +0300

    feat(content): add The Horse and His Rider passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -68,6 +68,7 @@ import aesopHareAndTortoise from "./aesop-hare-and-tortoise.json";
 import aesopHaresAndFrogs from "./aesop-hares-and-frogs.json";
 import aesopHawkKiteAndPigeons from "./aesop-hawk-kite-and-pigeons.json";
 import aesopHorseAndGroom from "./aesop-horse-and-groom.json";
+import aesopHorseAndHisRider from "./aesop-horse-and-his-rider.json";
 import aesopHoundAndFox from "./aesop-hound-and-fox.json";
 import aesopJackdawAndPigeons from "./aesop-jackdaw-and-pigeons.json";
 import aesopKidOnHousetop from "./aesop-kid-on-housetop.json";
@@ -369,6 +370,7 @@ export const rawPassages: unknown[] = [
   aesopHaresAndFrogs,
   aesopHawkKiteAndPigeons,
   aesopHorseAndGroom,
+  aesopHorseAndHisRider,
   aesopHoundAndFox,
   aesopJackdawAndPigeons,
   aesopKidOnHousetop,
