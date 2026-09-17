d119fced444b53e1d1c00d6f6fc946546b978f53
Rasmus Kuusmaa <77f315@gmail.com>
2026-09-18 00:19:10 +0300

    feat(content): add The Partridge and the Fowler passage

--- src/content/passages/index.ts
+++ src/content/passages/index.ts
@@ -87,6 +87,7 @@ import aesopOldWomanAndDoctor from "./aesop-old-woman-and-doctor.json";
 import aesopOldWomanAndWineJar from "./aesop-old-woman-and-wine-jar.json";
 import aesopOliveTreeAndFigTree from "./aesop-olive-tree-and-fig-tree.json";
 import aesopOxenAndAxletrees from "./aesop-oxen-and-axletrees.json";
+import aesopPartridgeAndFowler from "./aesop-partridge-and-fowler.json";
 import aesopPeacockAndCrane from "./aesop-peacock-and-crane.json";
 import aesopPeasantAndAppleTree from "./aesop-peasant-and-apple-tree.json";
 import aesopPomegranateAppleTreeAndBramble from "./aesop-pomegranate-apple-tree-and-bramble.json";
@@ -378,6 +379,7 @@ export const rawPassages: unknown[] = [
   aesopOldWomanAndWineJar,
   aesopOliveTreeAndFigTree,
   aesopOxenAndAxletrees,
+  aesopPartridgeAndFowler,
   aesopPeacockAndCrane,
   aesopPeasantAndAppleTree,
   aesopPomegranateAppleTreeAndBramble,
